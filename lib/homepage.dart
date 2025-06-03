bd1bd0403dbef2882a48088069dc42d3214a2a25
Satvik <satviksharma3123@gmail.com>
2025-06-03 13:19:31 +0530

    Update build configuration, enhance homepage layout, and add dependencies

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -4,6 +4,14 @@ class Home_Page extends StatelessWidget {
   const Home_Page({super.key});
   @override
   Widget build(BuildContext context) {
-    return Placeholder();
+    return Scaffold(
+      appBar: AppBar(title: Center(child: Text('Video App'))),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: Column(children: []),
+        ),
+      ),
+    );
   }
 }
